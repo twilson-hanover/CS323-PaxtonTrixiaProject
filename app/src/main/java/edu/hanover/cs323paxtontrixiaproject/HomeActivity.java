88057823e56521946db3fe2425c9c0387b6aecc5
yuzont19 <yuzont19@hanover.edu>
2018-05-23 10:26:01 -0400

    Added the next BlankActivity and used Intent to start the second Activity

---
+++ app/src/main/java/edu/hanover/cs323paxtontrixiaproject/HomeActivity.java
@@ -0,0 +1,14 @@
+package edu.hanover.cs323paxtontrixiaproject;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
--- app/src/main/java/edu/hanover/cs323paxtontrixiaproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323paxtontrixiaproject/MainActivity.java
@@ -2,6 +2,8 @@ package edu.hanover.cs323paxtontrixiaproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onEnter(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
