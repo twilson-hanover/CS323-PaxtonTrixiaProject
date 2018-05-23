a3f2080a15617a1fa751c591eace1bd53844aff6
yuzont19 <yuzont19@hanover.edu>
2018-05-23 10:57:32 -0400

    Added DogBreeds activity and created an intent to go to that activity

---
+++ app/src/main/java/edu/hanover/cs323paxtontrixiaproject/DogBreeds.java
@@ -0,0 +1,14 @@
+package edu.hanover.cs323paxtontrixiaproject;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DogBreeds extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog_breeds);
+    }
+
+}
--- app/src/main/java/edu/hanover/cs323paxtontrixiaproject/HomeActivity.java
+++ app/src/main/java/edu/hanover/cs323paxtontrixiaproject/HomeActivity.java
@@ -1,7 +1,9 @@
 package edu.hanover.cs323paxtontrixiaproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 public class HomeActivity extends Activity {
 
@@ -11,4 +13,9 @@ public class HomeActivity extends Activity {
         setContentView(R.layout.activity_home);
     }
 
+    //Call onFindDog() when button is clicked
+    public void onFindDog(View view) {
+        Intent intent = new Intent(this, DogBreeds.class);
+        startActivity(intent);
+    }
 }
