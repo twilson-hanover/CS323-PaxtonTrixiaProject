9f6f14f0cad9799cb24555bf43540b716492f954
yuzont19 <yuzont19@hanover.edu>
2018-05-23 10:47:00 -0400

    Added Settings and actually made it work!

--- app/src/main/java/edu/hanover/cs323paxtontrixiaproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323paxtontrixiaproject/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.view.Menu;
 
 public class MainActivity extends Activity {
 
@@ -17,5 +18,12 @@ public class MainActivity extends Activity {
     public void onEnter(View view) {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
