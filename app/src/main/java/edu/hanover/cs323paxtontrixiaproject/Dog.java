4d5a8a9c2765f5f2c1ac85f66256b3d72a8db9e2
yuzont19 <yuzont19@hanover.edu>
2018-05-23 13:16:47 -0400

    Added Dog.java Class

---
+++ app/src/main/java/edu/hanover/cs323paxtontrixiaproject/Dog.java
@@ -0,0 +1,41 @@
+package edu.hanover.cs323paxtontrixiaproject;
+
+public class Dog {
+
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    //dog is an array of Dog
+    public static final Dog[] dog = {
+            new Dog("Husky", "Loyal, Outgoing, Mischievous", R.drawable.husky),
+            new Dog("Labrador", "Friendly, Active, Outgoing", R.drawable.labrador),
+            new Dog("Corgi", "Affectionate, Smart, Alert", R.drawable.corgi),
+            new Dog("German Shepherd", "Confident, Courageous, Smart", R.drawable.german_shepherd),
+            new Dog("Pitbull", "Confident, Smart, Good-Natured", R.drawable.pitbull),
+            new Dog("Beagle", "Friendly, Curious, Merry", R.drawable.beagle)
+    };
+
+    //Each Drink has a name, description, and an image resource
+    private Dog(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
