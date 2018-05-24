8976ebf99e7ec0573aec77383b0c5e8465890caf
yuzont19 <yuzont19@hanover.edu>
2018-05-24 13:33:51 -0400

    Changed the main menu layout to work when on landscape!

--- app/src/main/java/edu/hanover/cs323paxtontrixiaproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323paxtontrixiaproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.view.Menu;
+import android.content.res.Configuration;
 
 public class MainActivity extends Activity {
 
